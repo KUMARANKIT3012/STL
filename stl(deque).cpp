88e2ce22a5bb7b08e7347bd0c38800d42983dbe6
Ankit Kumar <ankitkumar16729@gmail.com>
2024-10-28 19:48:40 +0530

    Add files via upload

---
+++ stl(deque).cpp
@@ -0,0 +1,26 @@
+// functions are similar to list:
+/*
+#include<iostream>
+#include<deque>
+using namespace std;
+int main(){
+    deque<int>d={1,2,3,4,5};
+
+   d.emplace_back(8); // add value to back
+    d.push_back(6); // add value to back
+    d.push_front(7); // add value to front
+    d.emplace_front(9); // add value to front
+    d.pop_back(); // delete the back value 
+    d.pop_front(); // delete the front value 
+
+    for(int val:d){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    cout<<d[3];
+    return 0;
+}
+// output = 7 1 2 3 4 5 8
+//          3 
+
+*/
